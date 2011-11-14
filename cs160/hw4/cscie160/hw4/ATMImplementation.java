995cafda7a96a30ab5650f75bbcbc84a8399fba1
Ellery Coleman <ellery.coleman@gmail.com>
2011-11-14 09:24:49 +0000

    Added javadocs.

--- cs160/hw4/cscie160/hw4/ATMImplementation.java
+++ cs160/hw4/cscie160/hw4/ATMImplementation.java
@@ -1,5 +1,11 @@
-
-
+/*----------------------------------------------------------------------------
+# File:		ATMImplementation.java
+# Date:		Mon Nov 14 03:08:07 GMT 2011
+# Author:	Ellery Coleman <ellerycoleman@fas.harvard.edu>
+# Abstract:	Implements ATM interface for cscie160, hw4.
+#-----------------------------------------------------------------------------
+# Revision: $Id: Elevator.java 50 2011-11-10 21:18:24Z ellery $
+#---------------------------------------------------------------------------*/
 package cscie160.hw4;
 
 
@@ -7,6 +13,7 @@ package cscie160.hw4;
 public class ATMImplementation implements ATM
 {
 
+    //--------------
     // Data Members
     //--------------
     private Account account;
@@ -14,8 +21,14 @@ public class ATMImplementation implements ATM
 
 
 
+    //-------------
     // Constructor
     //-------------
+
+
+   /**
+    * Default constructor to initialize the bank account.
+    */
     public ATMImplementation()
     {   account= new Account();
     }
@@ -23,20 +36,36 @@ public class ATMImplementation implements ATM
 
 
 
+
+    //-----------------
     // Method members
     //-----------------
+
+
+   /**
+    * Deposits the specified amount into the account.
+    */
     public void deposit(float amount) throws ATMException
     {   System.out.print("ATMImplementation.deposit() has been invoked.\n");
         Float balance= account.getBalance() + amount;
 	account.setBalance(balance);
     }
 
+
+
+   /**
+    * Withdraws the specified amount from the account.
+    */
     public void withdraw(float amount) throws ATMException
     {   System.out.print("ATMImplementation.withdraw() has been invoked.\n");
         Float balance= account.getBalance() - amount;
 	account.setBalance(balance);
     }
 
+
+   /**
+    * Returns the account balance.
+    */
     public Float getBalance() throws ATMException
     {   System.out.print("ATMImplementation.getBalance() has been invoked.\n");
         return account.getBalance();
--- cs160/hw4/cscie160/hw4/Account.java
+++ cs160/hw4/cscie160/hw4/Account.java
@@ -1,25 +1,59 @@
+/*----------------------------------------------------------------------------
+# File:		Account.java
+# Date:		Mon Nov 14 03:08:07 GMT 2011
+# Author:	Ellery Coleman <ellerycoleman@fas.harvard.edu>
+# Abstract:	Implements an Account class for cscie160, hw4.
+#-----------------------------------------------------------------------------
+# Revision: $Id: Elevator.java 50 2011-11-10 21:18:24Z ellery $
+#---------------------------------------------------------------------------*/
 package cscie160.hw4;
+
 
 
 public class Account
 {
+
+
+    //----------------
     // 1 Data Member
+    //----------------
     private Float balance;
 
 
 
+
+
+
+    //---------------
     // Constructor
+    //---------------
+
+   /**
+    * A default constructor that sets the account balance to 0.
+    */
     public Account()
     {   balance= 0.0F;
     }
 
 
+
+
+
+    //-----------------
     // Method Members
+    //-----------------
+
+   /**
+    * Returns the account balance.
+    */
     public Float getBalance()
     {   return balance;
     }
 
 
+   /**
+    * Sets the account balance according to the parameter.
+    */
     public void setBalance(Float b)
     {   balance= b;
     }
