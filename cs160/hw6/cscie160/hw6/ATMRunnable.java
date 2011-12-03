0f8f9aef3e347d1dbc48d98e0e002e4b0a3b2075
Ellery Coleman <ellery.coleman@gmail.com>
2011-12-03 19:15:39 +0000

    Implements the run method according to hw6 spec.

--- cs160/hw6/cscie160/hw6/ATMRunnable.java
+++ cs160/hw6/cscie160/hw6/ATMRunnable.java
@@ -10,13 +10,14 @@ package cscie160.hw6;
 
 
 import java.io.*;
+import java.util.*;
 
 
 /**
  * Implements ATMRunnable; a work order object for the threaded ATM.
  */
 
-public class ATMRunnable implements Runnable
+public class ATMRunnable
 {
 
 
@@ -57,11 +58,76 @@ public class ATMRunnable implements Runnable
     * through the ATM interface, and writes a status message to
     * the client.
     */
-    public void run()
-    {   printstream.println("THIS IS THE RUN METHOD:");
-        printstream.println("=======================");
+    public void run() throws ATMException
+    {   
+    
 
-    }
+	/*  The logic here is specific to our protocol.  We parse the string
+	 *  according to that protocol.
+	 */
+	        String commandLine= command;
+		PrintStream clientOut= printstream;
+
+
+		System.out.println("Server received command: " + commandLine);
+
+
+		// Break out the command line into String[]
+		StringTokenizer tokenizer = new StringTokenizer(commandLine);
+		String commandAndParam[] = new String[tokenizer.countTokens()];
+		int index = 0;
+		while (tokenizer.hasMoreTokens())
+		{
+			commandAndParam[index++] = tokenizer.nextToken();
+		}
+
+
+		// store the command
+		String command = commandAndParam[0];
+
+
+		// Dispatch BALANCE request without further ado.
+		if (command.equalsIgnoreCase(Commands.BALANCE.toString()))
+		{       System.out.println("el: Client wants to check balance...");
+			clientOut.println(atm.getBalance());
+			return;
+		}
+
+
+		// Must have 2nd arg for amount when processing DEPOSIT/WITHDRAW commands
+		if (commandAndParam.length < 2)
+		{   throw new ATMException("Missing amount for command \"" + command + "\"");
+		}
+
+
+
+                // execute deposit or withdraw command
+		try
+		{
+		    float amount = Float.parseFloat(commandAndParam[1]);
+		    if (command.equalsIgnoreCase(Commands.DEPOSIT.toString()))
+		    {
+			atm.deposit(amount);
+			return;
+		    }
+		    else if (command.equalsIgnoreCase(Commands.WITHDRAW.toString()))
+		    {
+			atm.withdraw(amount);
+			return;
+		    }
+		    else
+		    {
+			throw new ATMException("Unrecognized command: " + command);
+		    }
+		}
+		catch (NumberFormatException nfe)
+		{
+			throw new ATMException("Unable to make float from input: " + commandAndParam[1]);
+		}
+		// BALANCE command returned result above.  Other commands return null;
+      }
+
+
 
 
 
