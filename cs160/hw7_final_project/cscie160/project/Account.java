1141af51dfac6c45d6e93af2c44becad3f56baa4
Ellery Coleman <ellery.coleman@gmail.com>
2012-01-02 14:26:28 +0000

    Account interface.

---
+++ cs160/hw7_final_project/cscie160/project/Account.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------
+# File:		Account.java
+# Date:		Mon Nov 14 03:08:07 GMT 2011
+# Author:	Ellery Coleman <ellerycoleman@fas.harvard.edu>
+# Abstract:	Implements the Account interface for cscie160, final project.
+#-----------------------------------------------------------------------------
+# Revision: $Id: ATMImpl.java 65 2011-11-24 08:49:27Z ellery $
+#---------------------------------------------------------------------------*/
+package cscie160.project;
+import java.rmi.*;
+
+
+
+
+/**
+ * Interface for a bank Account.
+ */
+public interface Account extends Remote
+{
+    // Method Members
+    public Float getBalance() throws RemoteException;
+    public void deposit(Float f) throws RemoteException;
+    public void withdraw(Float f) throws RemoteException;
+    public void setBalance(Float b) throws RemoteException;
+    public int getAccountNumber() throws RemoteException;
+}
