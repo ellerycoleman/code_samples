94c31a8c01b9ccb2b924c0a6de8358b3df223147
Ellery Coleman <ellery.coleman@gmail.com>
2012-01-06 02:03:20 +0000

    Updated to run Ellery's tests instead of the prof's standard test suite.

--- cs160/hw7_final_project/cscie160/project/Client2.java
+++ cs160/hw7_final_project/cscie160/project/Client2.java
@@ -71,110 +71,6 @@
 
 
 
-//==============================================================================
-//  BEGIN -->  VERBATIM CLIENT CODE PROVIDED AND REQUIRED BY PROFESSOR SAWYER
-//==============================================================================
-
-   public static void testATM(ATM atm) {
-      if (atm!=null) {
-         printBalances(atm);
-         performTestOne(atm);
-         performTestTwo(atm);
-         performTestThree(atm);
-         performTestFour(atm);
-         performTestFive(atm);
-         performTestSix(atm);
-         performTestSeven(atm);
-         performTestEight(atm);
-         performTestNine(atm);
-         printBalances(atm);
-      }
-   }        
-   public static void printBalances(ATM atm) {        
-      try {
-         System.out.println("Balance(0000001): "+atm.getBalance(getAccountInfo(0000001, 1234)));
-         System.out.println("Balance(0000002): "+atm.getBalance(getAccountInfo(0000002, 2345)));
-         System.out.println("Balance(0000003): "+atm.getBalance(getAccountInfo(0000003, 3456)));
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-   public static void performTestOne(ATM atm) {       
-      try {
-         atm.getBalance(getAccountInfo(0000001, 5555));
-      } catch (Exception e) {
-         System.out.println("Failed as expected: "+e);
-      }
-   }
-   public static void performTestTwo(ATM atm) {       
-      try {
-         atm.withdraw(getAccountInfo(0000002, 2345), 500);
-      } catch (Exception e) {
-         System.out.println("Failed as expected: "+e);
-      }
-   }
-   public static void performTestThree(ATM atm) {        
-      try {
-         atm.withdraw(getAccountInfo(0000001, 1234), 50);
-      } catch (Exception e) {
-         System.out.println("Failed as expected: "+e);
-      }
-   }
-   public static void performTestFour(ATM atm) {         
-      try {
-         atm.deposit(getAccountInfo(0000001, 1234), 500);
-      } catch (Exception e) {
-         System.out.println("Unexpected error: "+e);
-      }
-   }
-   public static void performTestFive(ATM atm) {         
-      try {
-         atm.deposit(getAccountInfo(0000002, 2345), 100);
-      } catch (Exception e) {
-         System.out.println("Unexpected error: "+e);
-      }
-   }
-   public static void performTestSix(ATM atm) {       
-      try {
-         atm.withdraw(getAccountInfo(0000001, 1234), 100);
-      } catch (Exception e) {
-         System.out.println("Unexpected error: "+e);
-      }
-   }
-   public static void performTestSeven(ATM atm) {        
-      try {
-         atm.withdraw(getAccountInfo(0000003, 3456), 300);
-      } catch (Exception e) {
-         System.out.println("Unexpected error: "+e);
-      }
-   }
-   public static void performTestEight(ATM atm) {        
-      try {
-         atm.withdraw(getAccountInfo(0000001, 1234), 200);
-      } catch (Exception e) {
-         System.out.println("Failed as expected: "+e);
-      }
-   }
-   public static void performTestNine(ATM atm) {        
-      try {
-         atm.transfer(getAccountInfo(0000001, 1234),getAccountInfo(0000002, 2345), 100);
-      } catch (Exception e) {
-         System.out.println("Unexpected error: "+e);
-      }
-   }
-
-//==============================================================================
-//   END -->  VERBATIM CLIENT CODE PROVIDED AND REQUIRED BY PROFESSOR SAWYER
-//==============================================================================
-
-
-
-
-
-
-
-
-
      public static void main(String[] args) throws RemoteException, NamingException, ATMException, NSFException, SecurityException 
     {   System.out.println("\nStarting test harness...\n");
 
@@ -202,11 +98,8 @@
 	      // of the ATM.
 	      Client2 c2= new Client2();
 	      atm.addObserver(c2);
-	      testATM(atm);
 
 
-
-/*
 
            try {
               // Specify account credentials
@@ -274,8 +167,6 @@
 	   {   sece.printStackTrace();
 	       System.exit(1);
 	   }
-
-*/
 
         }
 	System.exit(0);
