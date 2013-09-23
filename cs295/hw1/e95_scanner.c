f652216d7f294cd9f9b8d9ebb3fe43d3dcc7c29e
Ellery Coleman <ellery.coleman@gmail.com>
2013-09-23 01:36:54 +0000

    Recognizes octal char constants and properly displays their int values.

--- cs295/hw1/e95_scanner.c
+++ cs295/hw1/e95_scanner.c
@@ -21,8 +21,7 @@ void *yylval;
 extern int cvalue;
 
 
-int get_char_val(char *);
-
+int get_octal_val(char *);
 
 
 int main(int argc, char **argv)
@@ -72,6 +71,7 @@ int main(int argc, char **argv)
             case CHARACTER_CONSTANT_OCTAL:
                printf("line: %-3d", yylineno);
                printf(" token: %-15s", yytext);
+	       cvalue= get_octal_val(yytext);
                printf(" name: %-18s", token_def_map[token].name);
                printf("  dec_val: %-9d", cvalue);
                printf("type: %-15s\n", token_def_map[token].type);
@@ -93,4 +93,48 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+
+
+
+
+int get_octal_val(char *str)
+{   
+
+#ifdef DEBUG
+    printf("\n\n\n");
+    printf("input string: |%s|\n\n", str);
+#endif
+
+    /* initialization */
+    int len= strlen(str);
+    char oct[4]= "000";
+    int i,j,sum;
+
+
+
+#ifdef DEBUG
+    printf("String Length: %d\n", len);
+#endif
+
+    /* read octal digits into separate string */
+    j=2;
+    for(i=len-1; i>=0; i--)
+    {   if( isdigit(str[i]) )
+        {   oct[j--]= str[i];
+	}
+    } 
+
+
+#ifdef DEBUG
+    printf("Octal Digits: %s\n", oct);
+#endif
+
+    /* calculate decimal value of octal string */
+    sum= (int) strtol(oct,NULL,8);
+
+    return sum;
+}
+
+
 
