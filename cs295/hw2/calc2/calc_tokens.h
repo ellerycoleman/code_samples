2c3b78d2928e9aa0776eca6bf3874e2404c65317
Ellery Coleman <ellery.coleman@gmail.com>
2013-09-30 22:39:38 +0000

    need a calc_tokens.h file in order to compile parser.

---
+++ cs295/hw2/calc2/calc_tokens.h
@@ -0,0 +1,47 @@
+/*============================================================================
+| File:		calc_tokens.h
+| Date:		Tue Sep 24 15:07:52 GMT 2013
+| Author:	ellerycoleman@fas.harvard.edu
+| Abstract:	Token definitions for calculator.
++-----------------------------------------------------------------------------
+| Revision: $Id$
++===========================================================================*/
+
+
+
+#ifndef _CALC_TOKENS_H
+#define _CALC_TOKENS_H  1
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define MAX_TOKEN_DEFS 500
+
+
+
+/* Simple Operators   */
+#include "bison_parser.tab.h"
+
+
+/* creating token structure */
+typedef struct calc_token 
+{   char *name;
+    char *type;
+} calc_token;
+
+
+
+/* defining a token map to hold all token definitions */
+calc_token token_def_map[MAX_TOKEN_DEFS];
+
+
+
+/* function prototypes */
+void init_token_definition_map(void);
+
+
+#endif
