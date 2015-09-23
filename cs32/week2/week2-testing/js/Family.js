cf1ee4eebb6fa046fde30ea4d949a64771c472bf
Ellery Coleman <ellery.coleman@gmail.com>
2015-09-23 17:24:26 +0000

    - Working version of Family list app, with debug messages.

--- cs32/week2/week2-testing/js/Family.js
+++ cs32/week2/week2-testing/js/Family.js
@@ -1,7 +1,7 @@
 
 var Family = function(){
 	// Initialize
-	this.family = JSON.parse(sessionStorage.getItem('family') || [];
+	this.family = JSON.parse(sessionStorage.getItem('family')) || [];
     this.listenAddPerson();
 
 	this.updateList();
@@ -9,13 +9,14 @@ var Family = function(){
 
 
 Family.prototype.listenAddPerson = function()
-{
-	var that= this;
-	$('#addPersonForm').on('submit', function(event){
-		                                that.addPerson($('#person').val());
-									    $('#person').val('');
-									    return false;
-	                                  });
+{   var that= this;
+	$('#addPersonForm').on('submit', 
+	  function(event)
+	  {   that.addPerson($('#person').val());
+		  $('#person').val('');
+		   return false;
+      }
+	 );
 };
 
 Family.prototype.addPerson = function(person)
@@ -23,12 +24,37 @@ Family.prototype.addPerson = function(person)
 	this.updateList();
 };
 
-Family.prototype.removeSong = function(index)
-{
-    this.playlist.splice(index,1);
-	this.updatePlaylist();
+Family.prototype.removePerson = function(index)
+{   console.log("removePerson() was called with index = '", index, "'");
+    this.family.splice(index,1);
+	this.updateList();
 };
 
+Family.prototype.updateList = function()
+{   sessionStorage.setItem('family',JSON.stringify(this.family));
+	this.updateFamilyDom();
+}
 
+Family.prototype.updateFamilyDom = function(){
+    var that= this;
+	var familyDom = this.family.map(
+		function(person,index)
+		{   console.log(  "updateFamilyDom.familyDom() was called with"
+		                + " person='" + person + "'"
+					    + " index='"  + index  + "'"
+					   );	
+			var removeButton = document.createElement("button");
+		    removeButton.appendChild(document.createTextNode("remove"));
+		    $(removeButton).click(function(){ that.removePerson(index); });
+		    $(removeButton).addClass("btn");
 
+		    var li = document.createElement('li');
+		    li.appendChild(document.createTextNode(person));
+		    li.appendChild(removeButton);
+		    return li;
+	    }
+	);
 
+	$('#currentList').html(familyDom);
+
+};
