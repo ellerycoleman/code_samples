53d2a6809f57d75c9f676678571d011613ec3e58
Ellery Coleman <ellery.coleman@gmail.com>
2015-09-23 17:58:31 +0000

    Added additional debugging messages.

--- cs32/week2/week2-testing/js/Family.js
+++ cs32/week2/week2-testing/js/Family.js
@@ -20,29 +20,39 @@ Family.prototype.listenAddPerson = function()
 };
 
 Family.prototype.addPerson = function(person)
-{   this.family.push(person);
+{   console.log("addPerson() was call with " + arguments.length
+		        + " args; person='" + person + "'");
+	this.family.push(person);
 	this.updateList();
 };
 
 Family.prototype.removePerson = function(index)
-{   console.log("removePerson() was called with index = '", index, "'");
+{   console.log("removePerson() was called with " + arguments.length  
+		        + " args; index = '" + index + "'");
     this.family.splice(index,1);
 	this.updateList();
 };
 
 Family.prototype.updateList = function()
-{   sessionStorage.setItem('family',JSON.stringify(this.family));
+{   console.log("updateList() called with " + arguments.length + " args");
+	sessionStorage.setItem('family',JSON.stringify(this.family));
 	this.updateFamilyDom();
 }
 
 Family.prototype.updateFamilyDom = function(){
+	console.log("updateFamilyDom() called with " + arguments.length + " args");
     var that= this;
 	var familyDom = this.family.map(
 		function(person,index)
-		{   console.log(  "updateFamilyDom.familyDom() was called with"
-		                + " person='" + person + "'"
+		{   console.log(  "updateFamilyDom.familyDom() was called with "
+		                + arguments.length + " args; "
+			            + " person='" + person + "'"
 					    + " index='"  + index  + "'"
 					   );	
+		    var i;
+			for(i=0; i<arguments.length; i++)
+		    {   console.log("args[" + i + "]:" + arguments[i]);
+            }
 			var removeButton = document.createElement("button");
 		    removeButton.appendChild(document.createTextNode("remove"));
 		    $(removeButton).click(function(){ that.removePerson(index); });
